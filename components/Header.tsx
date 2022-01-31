c464c77327294e0d04d55f35ad8c0ee3a59c8a56
Henry <henrynoowah@gmail.com>
2022-01-31 23:10:55 +0900

    [ADD] Authenticated Headner user menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 import styled from "styled-components";
 import useModal from "../hooks/useModal";
 import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
 import AirbnbLogoTextIcon from "../public/static/svg/logo/logo_text.svg";
 import { useSelector } from "../store";
 import palette from "../styles/palette";
-import SignUpModal from "./auth/SignUpModal";
 import HamburgerIcon from "../public/static/svg/header/hamburger.svg";
 import { useDispatch } from "react-redux";
 import { authActions } from "../store/auth";
 import AuthModal from "./auth/AuthModal";
+import OutsideClickHander from "react-outside-click-handler";
 
 const Container = styled.div`
   position: sticky;
@@ -30,6 +30,38 @@ const Container = styled.div`
     align-items: center;
     .header-logo {
       margin-right: 6px;
+    }
+  }
+
+  .header-logo-wrapper + div {
+    position: relative;
+  }
+
+  .header-usermenu {
+    position: absolute;
+    right: 0;
+    top: 52px;
+    width: 240px;
+    padding: 8px 0;
+    box-shadow: 0 2px 16px rgba(0, 0, 0, 0.12);
+    border-radius: 8px;
+    background-color: white;
+    li {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 42px;
+      padding: 0 16px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${palette.gray_f7};
+      }
+    }
+    .header-usermenu-divider {
+      width: 100%;
+      height: 1px;
+      margin: 8px 0;
+      background-color: ${palette.gray_dd};
     }
   }
 
@@ -86,6 +118,7 @@ const Container = styled.div`
 `;
 
 const Header: FC = () => {
+  const [isUserMenuOpened, setIsUserMenuOpened] = useState(false);
   const { openModal, ModalPortal, closeModal } = useModal();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
@@ -123,14 +156,42 @@ const Header: FC = () => {
         </div>
       )}
       {user.isLogged && (
-        <button className="header-user-profile" type="button">
-          <HamburgerIcon />
-          <img
-            className="header-user-profile-image"
-            src={user.profileImage}
-            alt=""
-          />
-        </button>
+        <OutsideClickHander
+          onOutsideClick={() => {
+            if (isUserMenuOpened) {
+              setIsUserMenuOpened(false);
+            }
+          }}
+        >
+          <button
+            className="header-user-profile"
+            type="button"
+            onClick={() => setIsUserMenuOpened(!isUserMenuOpened)}
+          >
+            <HamburgerIcon />
+            <img
+              className="header-user-profile-image"
+              src={user.profileImage}
+              alt=""
+            />
+          </button>
+          {isUserMenuOpened && (
+            <ul className="header-usermenu">
+              <li>숙소 관리</li>
+              <Link href="/room/register/building">
+                <a role="presentation" onClick={() => {}} />
+              </Link>
+              <li>숙소 등록하기</li>
+              <Link href="/room/register/building">
+                <a role="presentation" onClick={() => {}} />
+              </Link>
+              <div className="header-usermenu-divider" />
+              <li role="presentation" onClick={() => {}}>
+                로그아웃
+              </li>
+            </ul>
+          )}
+        </OutsideClickHander>
       )}
       {ModalPortal && (
         <ModalPortal>
