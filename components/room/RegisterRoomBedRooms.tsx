92b49e1a2d76e9b753ceaf4d80dcb441c0e2ce10
Henry <henrynoowah@gmail.com>
2022-02-06 01:04:23 +0900

    [ADD] Component RegisterRoomBedType.tsx

--- components/room/RegisterRoomBedRooms.tsx
+++ components/room/RegisterRoomBedRooms.tsx
@@ -9,6 +9,7 @@ import palette from "../../styles/palette";
 import Button from "../common/Button";
 import Counter from "../common/Counter";
 import Selector from "../common/Selector";
+import RegisterRoomBedTypes from "./RegisterRoomBedTypes";
 import RegisterRoomFooter from "./RegisterRoomFooter";
 
 const Container = styled.div`
@@ -46,25 +47,8 @@ const Container = styled.div`
     max-width: 400px;
     word-break: keep-all;
   }
-  .register-room-bedroom {
-    width: 100%;
-    padding: 28px 0;
-    border-top: 1px solid ${palette.gray_dd};
-    &:last-child {
-      border-bottom: 1px solid ${palette.gray_dd};
-    }
-  }
-  .register-room-bed-type-top {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .register-room-bed-type-bedroom-texts {
-    margin-bottom: 28px;
-  }
-  .register-room-bed-type-bedroom {
-    font-size: 19px;
-    color: ${palette.gray_48};
+  .register-room-bed-type-list-wrapper {
+    width: 548px;
   }
 `;
 
@@ -126,23 +110,9 @@ const RegisterRoomBedRooms: FC = () => {
         각 침실에 놓인 침대유형을 명시하면 숙소에 침대가 어떻게 구비되어 있는지
         게스트가 잘 파악할 수 있습니다.
       </p>
-      <div className="register-room-bedroom-count-wrapper">
+      <div className="register-room-bed-type-list-wrapper">
         {bedList.map((bedroom) => (
-          <div className="register-room-bedroon">
-            <div className="register-room-bed-type-top">
-              <div className="register-room-bed-type-bedroom-texts">
-                <p className="register-room-bed-type-bedroom">
-                  {bedroom.id} 번 침실
-                </p>
-                <p className="register-room-bed-type-bedroom-counts">
-                  침대 0개
-                </p>
-              </div>
-              <Button styleType="register" color="white">
-                침대 추가하기
-              </Button>
-            </div>
-          </div>
+          <RegisterRoomBedTypes bedroom={bedroom} />
         ))}
       </div>
     </Container>
